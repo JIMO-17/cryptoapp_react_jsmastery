561314b874f653b2113f25ae73a4aead1d2e4229
Josue Isai Mesa Mora <jies1702@gmail.com>
2022-01-21 19:02:19 -0500

    se cambia version de las dependencias y se crea los componentes base y se inicia las rutas

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+//manera de importar componentes de manera eficiente
+export { default as Navbar } from './Navbar';
+export { default as Cryptocurrencies } from './Cryptocurrencies';
+export { default as CryptoDetails } from './CryptoDetails';
+export { default as Exchanges } from './Exchanges';
+export { default as Homepage } from './Homepage';
+export { default as News } from './News';
